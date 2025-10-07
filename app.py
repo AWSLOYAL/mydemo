1e7990208e324c7cc9648e6cf8b539e0df9d5777
AWSLOYAL <modernlve23@gmail.com>
2025-10-07 07:48:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-FROM python:3.9-slim-buster
-WORKDIR /app
-COPY . .
-RUN pip install -r requirements.txt
-CMD ["python", "app.py"]
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello from Flask!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
